b25721a926746689f3e48092a931a61dc7e18294
feng <1285636209@qq.com>
2026-01-27 15:10:19 +0800

    mini db v0

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,60 @@
 #include <cstring>
 #include <iostream>
 
+#include "storage/buffer_pool.h"
 #include "storage/disk_manager.h"
+#include "storage/table_heap.h"
+#include <sstream>
+#include <string>
+
+using namespace mini;
+
+void ExecuteSQL(const std::string &sql, TableHeap &table_heap) {
+  std::istringstream iss(sql);
+  std::string cmd;
+  iss >> cmd;
+
+  if (cmd == "insert") {
+    int id, value;
+    if (!(iss >> id >> value)) {
+      std::cerr << "syntax error: insert <id> <value>\n";
+      return;
+    }
+
+    Row row{id, value};
+    RID rid = table_heap.InsertRow(row);
+
+    std::cout << "inserted: page=" << rid.page_id << " slot=" << rid.slot_id
+              << "\n";
+    return;
+  }
+
+  if (cmd == "select") {
+    table_heap.Scan([](const RID &rid, const Row &row) {
+      std::cout << "page=" << rid.page_id << " slot=" << rid.slot_id
+                << " | id=" << row.id << " value=" << row.value << "\n";
+    });
+    return;
+  }
+
+  std::cerr << "unknown command: " << cmd << "\n";
+}
 
 int main() {
-  mini::DiskManager dm("mini.db");
+  DiskManager dm("mini.db");
+  BufferPool buffer_pool(100, &dm);
+  TableHeap table(&buffer_pool);
 
-  mini::Page out;
-  const char *msg = "page-0";
-  std::memcpy(out.data.data(), msg, std::strlen(msg));
-  dm.WritePage(0, out);
-
-  msg = "page-1";
-  std::memcpy(out.data.data(), msg, std::strlen(msg));
-  dm.WritePage(100, out);
-
-  mini::Page in;
-  dm.ReadPage(0, in);
-  std::cout << "read: " << reinterpret_cast<const char *>(in.data.data())
-            << "\n";
-  dm.ReadPage(100, in);
-  std::cout << "read: " << reinterpret_cast<const char *>(in.data.data())
-            << "\n";
+  std::string line;
+  while (true) {
+    std::cout << "mini-db> ";
+    if (!std::getline(std::cin, line)) {
+      break;
+    }
+    if (line == "exit" || line == "quit") {
+      break;
+    }
+    ExecuteSQL(line, table);
+  }
   return 0;
 }
