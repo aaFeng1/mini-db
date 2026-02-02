38b776aa713096fe3757eee593ca976c34e3a2ad
feng <1285636209@qq.com>
2026-02-02 17:50:15 +0800

    fix clean script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -5,20 +5,18 @@ BUILD_DIR=build
 GDB_BUILD_DIR=build-debug
 
 if [ ! -d "$BUILD_DIR" ]; then
-  echo "[clean] build/ 不存在，看起来很干净"
-  exit 0
+  echo "[clean] build/ not found"
 else
-  echo "[clean] 清理 $BUILD_DIR ..."
+  echo "[clean] $BUILD_DIR ..."
   rm -rf "$BUILD_DIR"
-  echo "[clean] 完成"
+  echo "[clean] finished"
 fi
 
 if [ ! -d "$GDB_BUILD_DIR" ]; then
-  echo "[clean] build-debug/ 不存在，看起来很干净"
-  exit 0
+  echo "[clean] build-debug/ not found"
 else
-  echo "[clean] 清理 $GDB_BUILD_DIR ..."
+  echo "[clean] $GDB_BUILD_DIR ..."
   rm -rf "$GDB_BUILD_DIR"
-  echo "[clean] 完成"
+  echo "[clean] finished"
 fi
 
